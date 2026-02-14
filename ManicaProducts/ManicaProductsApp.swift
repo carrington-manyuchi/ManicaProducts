3f0697622b1a2a29144d3912a26a86a11a4fc419
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2026-02-14 08:59:56 +0200

    Initial Commit

---
+++ ManicaProducts/ManicaProductsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManicaProductsApp.swift
+//  ManicaProducts
+//
+//  Created by Manyuchi, Carrington C on 2026/02/14.
+//
+
+import SwiftUI
+
+@main
+struct ManicaProductsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
